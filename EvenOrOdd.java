68e0a522a072358c186a7942e83853e2e537abb5
srilekhavelishoju <92663619+srilekhavelishoju@users.noreply.github.com>
2021-10-17 12:28:11 +0530

    Create EvenOrOdd.java

---
+++ EvenOrOdd.java
@@ -0,0 +1,16 @@
+import java.io.*;
+class EvenOrOdd
+{
+  public static void main(Sting args[])thros IOException
+  {
+    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    int n,rem;
+    System.out.println("Enter n value:");
+    n=Integer.parseInt(br.readLine());
+    rem=n%2;
+    if(rem==0)
+      System.out.println("even number");
+    else
+      System.out.println("odd number");
+  }
+}
