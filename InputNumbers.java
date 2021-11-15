86d671bd0e336eb5a8b0f87d07b9cd934b72161a
srilekhavelishoju <92663619+srilekhavelishoju@users.noreply.github.com>
2021-11-15 13:31:13 +0530

    InputNumbers

---
+++ InputNumbers.java
@@ -0,0 +1,33 @@
+package Edyodaprograms;
+import java.util.*;
+public class InputNumbers
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number:");
+        int n = scanner.nextInt();
+        int n1 = n,n2 = n;
+        int b = n1%10,a = n2/10;
+        String[] single_digits = new String[]
+                {"zero","one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+        String[] two_digits = new String[]
+                {"","ten","eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+        String[] tens_multiple = new String[]
+                {"","","twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
+        if(a==1)
+        {
+            System.out.println(two_digits[b + 1]);
+        }
+        else if(b==0)
+
+            System.out.println(tens_multiple[a]);
+
+        else
+
+           System.out.println(tens_multiple[a]+"-"+single_digits[b]);
+
+
+    }
+
+}
